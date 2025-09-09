81719e2b8b6a1c404ab7a5c2add81a3dc1d3d4cb
carlos <robertorsilva.krlos@gmail.com>
2025-09-09 15:14:25 -0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -46,6 +46,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, cleanupOpenApiDoc(document));
 
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
